475573075e5e6facfb7dadad35b1b2b76af32891
benoitbusnardo <benoit@makeprops.com>
2025-06-07 20:05:18 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+// import { AppRoutes } from './AppRoutes';
+// import { useTranslation } from 'react-i18next';
+// import { AuthProvider } from './hooks/useAuth';
+// import { ResetKeyContext } from './hooks/useResetKey';
+// import { useState } from 'react';
+import React from "react";
+
+function App() {
+    // const { t, i18n } = useTranslation();
+    // const [resetKey, setResetKey] = useState(0);
+
+    return (
+        <h1>test</h1>
+    );
+}
+
+export default App;
---
+++ src/main.tsx
@@ -0,0 +1,10 @@
+// import "./utils/i18n.ts";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import React from "react";
+// import "./index.css";
+
+createRoot(document.getElementById("root")!).render(
+  <App />
+);
+
