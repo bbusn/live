aa7c258260690f1aa9a9262f36d6d902a1ea4041
benoitbusnardo <benoit@makeprops.com>
2025-06-11 01:23:03 +0200

    feat: add live text in layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -7,11 +7,13 @@ import { AUTH_STATUS } from '../utils/auth';
 import { User } from '../objects/User';
 import ROUTES from '../constants/routes';
 import Statistics from './Statistics';
+import { useTranslation } from 'react-i18next';
 // import { useResetKey } from '../hooks/useResetKey';
 
 const Layout = () => {
     const { status } = useAuth();
     const navigate = useNavigate();
+    const { t } = useTranslation();
     // const { resetKey } = useResetKey();
 
     useEffect(() => {
@@ -25,6 +27,10 @@ const Layout = () => {
         <div className='w-full h-full min-h-[50vh] flex flex-col sm:flex-row sm:justify-around sm:items-start items-center justify-start 2xl:max-w-[1800px]'>
             <NavigationBar />
             <Statistics />
+            <div className='w-max fixed top-[25px] sm:top-[50px] left-[82.5%] sm:left-[90%] -translate-x-1/2 flex-1 flex justify-center items-center'>
+                <div className='w-3.5 h-3.5 rounded-full animate-pulse bg-[#E43F3F] group-hover transitions active:scale-95 hover:brightness-125'></div>
+                <span className='text-base text-white ml-2'>{t('navigation.live')}</span>
+            </div>
             <Outlet />
         </div>
     );
