751bd47fbbf7763f5da08a709067f2e3916d22c1
benoitbusnardo <benoit@makeprops.com>
2025-06-13 09:03:12 +0200

    fix: remove donations for 50 euros from achievements

--- src/constants/achievements.ts
+++ src/constants/achievements.ts
@@ -2,7 +2,6 @@ export const ACHIEVEMENTS = {
     SETTINGS_SHOW_SKILLS_FIRST_CLICK: 'settings_show_skills_first_click',
     SETTINGS_SWITCH_TO_ENGLISH: 'settings_switch_to_english',
     SETTINGS_TRY_TO_QUIT: 'settings_try_to_quit',
-    DONATIONS_50_EUROS: 'donations_50_euros',
     DONATIONS_100_EUROS: 'donations_100_euros',
     DONATIONS_200_EUROS: 'donations_200_euros',
     DONATIONS_500_EUROS: 'donations_500_euros',
@@ -30,7 +29,6 @@ export const ACHIEVEMENTS_TIME = {
 } as const;
 
 export const ACHIEVEMENTS_DONATIONS = {
-    [ACHIEVEMENTS.DONATIONS_50_EUROS]: 50,
     [ACHIEVEMENTS.DONATIONS_100_EUROS]: 100,
     [ACHIEVEMENTS.DONATIONS_200_EUROS]: 200,
     [ACHIEVEMENTS.DONATIONS_500_EUROS]: 500,
